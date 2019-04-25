dfe6245f75ebf5bfd7b54a5fa9504b37243921e7
Dauto98 <dauto98@gmail.com>
2019-04-25 23:01:22 +0700

    add socket

--- src/index.js
+++ src/index.js
@@ -6,9 +6,32 @@ function handleSubmit(e) {
   if (username && password) {
     fetch("http://localhost:3000/api/login", {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify({ username, password })
     });
   }
 }
 
 document.querySelector(".submitButton").addEventListener("click", handleSubmit);
+
+// socket things
+document.querySelector(".submitSocket").addEventListener("click", sendSocket);
+
+let socket = io('http://localhost:3000/socket'); // change url here
+
+function sendSocket() {
+  const eventName = document.querySelector(".eventName").value;
+  console.log(eventName);
+  const payload = document.querySelector(".payload").value || '';
+  console.log(payload);
+  if (eventName) {
+    socket.emit(eventName, payload);
+  }
+}
+
+// change event name here to whatever you want
+socket.on("some event name", (data) => {
+  console.log(data);
+})
